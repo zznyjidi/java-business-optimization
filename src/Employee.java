09bcb3c7ca3bbbbe2eb9d864979db66bf97547f8
hellowoll <zhengwilson7@gmail.com>
2026-02-19 11:39:27 -0500

    employee getters

--- src/Employee.java
+++ src/Employee.java
@@ -4,14 +4,38 @@ public class Employee{
     int speed;
     int coverage;
     int fulfilment;
-    String salary;
+    int salary;
 
-    public Employee(String name, String jobtitle, int speed, int coverage, int fulfilment, String salary){
+    public Employee(String name, String jobtitle, int speed, int coverage, int fulfilment, int salary){
         this.name = name;
         this.jobtitle = jobtitle;
         this.speed = speed;
         this.coverage = coverage;
         this.fulfilment = fulfilment;
         this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getJobTitle(){
+        return jobtitle;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public int getCoverage(){
+        return coverage;
+    }
+
+    public int getFulfilment(){
+        return fulfilment;
+    }
+
+    public int getSalary(){
+        return salary;
     }
 }
