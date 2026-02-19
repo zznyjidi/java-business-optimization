ea5e066e86385e2279bfe1a542d53d7e23f179e0
hellowoll <zhengwilson7@gmail.com>
2026-02-19 12:07:30 -0500

    to string employee

--- src/Employee.java
+++ src/Employee.java
@@ -38,4 +38,18 @@ public class Employee{
     public int getSalary(){
         return salary;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Employee{");
+        sb.append("name=").append(name);
+        sb.append(", jobtitle=").append(jobtitle);
+        sb.append(", speed=").append(speed);
+        sb.append(", coverage=").append(coverage);
+        sb.append(", fulfilment=").append(fulfilment);
+        sb.append(", salary=").append(salary);
+        sb.append('}');
+        return sb.toString();
+    }
 }
