835437341e89265af9d7cfb8265fcb532d3e1a92
hellowoll <zhengwilson7@gmail.com>
2026-02-19 12:29:24 -0500

    i added spaces

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,4 @@
-public class Employee{
+public class Employee {
     String name;
     String jobtitle;
     int speed;
@@ -6,7 +6,7 @@ public class Employee{
     int fulfilment;
     int salary;
 
-    public Employee(String name, String jobtitle, int speed, int coverage, int fulfilment, int salary){
+    public Employee(String name, String jobtitle, int speed, int coverage, int fulfilment, int salary) {
         this.name = name;
         this.jobtitle = jobtitle;
         this.speed = speed;
@@ -15,27 +15,27 @@ public class Employee{
         this.salary = salary;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getJobTitle(){
+    public String getJobTitle() {
         return jobtitle;
     }
 
-    public int getSpeed(){
+    public int getSpeed() {
         return speed;
     }
 
-    public int getCoverage(){
+    public int getCoverage() {
         return coverage;
     }
 
-    public int getFulfilment(){
+    public int getFulfilment() {
         return fulfilment;
     }
 
-    public int getSalary(){
+    public int getSalary() {
         return salary;
     }
 
