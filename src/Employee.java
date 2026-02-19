4a050dfb7e10e2e998f9d48a854db3ad1e5605a1
hellowoll <zhengwilson7@gmail.com>
2026-02-19 11:29:32 -0500

    this is the employee object code

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee{
+    String name;
+    String jobtitle;
+    int speed;
+    int coverage;
+    int fulfilment;
+    String salary;
+
+    public Employee(String name, String jobtitle, int speed, int coverage, int fulfilment, String salary){
+        this.name = name;
+        this.jobtitle = jobtitle;
+        this.speed = speed;
+        this.coverage = coverage;
+        this.fulfilment = fulfilment;
+        this.salary = salary;
+    }
+}
