0ac47b67ad6c22279438d3b6a59bcfaaed79f1ab
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-03-03 23:59:45 -0500

    fix: check for not enough budget

--- src/App.java
+++ src/App.java
@@ -6,12 +6,17 @@ import employee.Employee;
 import employee.ReadFile;
 import filters.FilterReplacements;
 import jobs.TeamBuilder;
+import score.Score;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
         List<Employee> employees = ReadFile.readEmployeeFile(new File("people2.csv"));
         TeamBuilder builder = new TeamBuilder(employees, Integer.parseInt(IO.readln("Budget> ")));
         builder.applyFilter(new FilterReplacements());
-        IO.println(builder.buildTeam());
+        Score result = builder.buildTeam();
+        if (result.isZero())
+            IO.println("Not Enough Budget. ");
+        else
+            IO.println(result);
     }
 }
